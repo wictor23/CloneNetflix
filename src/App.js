b81e29fc8fbf30ef6dfc3abbf315c2b9a21df7ce
Wictor DIniz <wictor.diniz@gmail.com>
2020-11-29 16:07:53 -0300

    Set list loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{ useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieRow from './components/MovieRow';
 import Featuremovie from './components/Featuremovie';
 import Tmdb from './Tmdb';
@@ -6,13 +6,13 @@ import './App.css';
 import Header from './components/Header';
 
 export default () => {
- 
+
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
-  
-  useEffect(()=>{
-    const loadAll = async() => {
+
+  useEffect(() => {
+    const loadAll = async () => {
       /*
         Pegando a lista de filmes
       */
@@ -22,45 +22,52 @@ export default () => {
         Filme em destaque
       */
       let originals = list.filter(i => i.slug === "originals");
-      let listaFilme = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let listaFilme = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let filme = originals[0].items.results[listaFilme];
-      let filmeInfo = await Tmdb.getMovieInfo(filme.id,'tv');
+      let filmeInfo = await Tmdb.getMovieInfo(filme.id, 'tv');
       setFeaturedData(filmeInfo);
 
     }
     loadAll();
   }, [])
-    useEffect(() =>{
-      const scrollListener = () =>{
+  useEffect(() => {
+    const scrollListener = () => {
 
-        if(window.scrollY > 10){
-          setBlackHeader(true);
-        }else{
-          setBlackHeader(false);
-        }
-
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
       }
 
-        window.addEventListener('scroll',scrollListener);
-      return ()=>{
-        window.removeEventListener('scroll',scrollListener);
-      }
-    },[]);
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
 
   return (
     <div className="page">
       <Header black={blackHeader} />
       {featuredData &&
-        <Featuremovie item={featuredData}/>
+        <Featuremovie item={featuredData} />
       }
       <section className="lists">
-        {movieList.map((item,key)=>(
-            <MovieRow key={key} title={item.title} items={item.items}/>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
-     </section>
-     <footer>
-       Feito para portifólio teste automatizado, Diniz <span className="icon" role="img" aria-label="phone">🎧</span>
-     </footer>
+      </section>
+      <footer>
+        Feito para portifólio teste automatizado, Diniz <span className="icon" role="img" aria-label="phone">🎧</span>
+      </footer>
+
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="loading"></img>
+        </div>
+      }
     </div>
   )
 }
